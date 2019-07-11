b1cf49549d024a0bb8df14df297e01bd5787555e
Christoph Schulz <schulzcbs@gmail.com>
2019-07-12 00:49:43 +0200

    Cleanup, add mapping, add tests.

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,49 @@ use std::cmp::Ordering;
 
 /// Regression result.
 pub struct Regression {
-    /// Number of points aggregated.
-    pub num: Vec<usize>,
-    /// Aggregated value.
-    pub val: Vec<f64>,
+    /// Aggregated weights, i.e., the number of points if all weights were one.
+    pub weights: Vec<f64>,
+    /// Aggregated values.
+    pub values: Vec<f64>,
 }
 
 /// Performs isotonic regression using the Pool-Adjacent-Violators-Algorithm (PAVA).
-pub fn pava(values: Vec<f64>, ordering: Ordering) -> Regression {
+///
+/// The algorithm minimizes $$\sum_{i=1}^{n} w_{i}(\hat{x}_i - x_i)^2$$ so that
+/// $$\hat{x}_i \leq \hat{x}_{i+1}$$, or respectively $$\hat{x}_i \geq \hat{x}_{i+1}$$.
+pub fn pava(values: &[f64], weights: &[f64], ordering: Ordering) -> Regression {
     assert!(ordering != Ordering::Equal, "Requires a total order");
-    let mut num = vec![0; values.len()];
-    let mut val = vec![0.0; values.len()];
-    num[0] = 1;
-    val[0] = values[0];
+    let mut w = vec![0.0; values.len()];
+    let mut x = vec![0.0; values.len()];
+    x[0] = values[0];
+    w[0] = weights[0];
     let mut j = 0;
+    let mut s = vec![0; values.len()];
     for i in 1..values.len() {
         j += 1;
-        val[j] = values[i];
-        num[j] = 1;
-        while (j > 0) && val[j - 1].partial_cmp(&val[j]).unwrap() == ordering {
-            val[j - 1] = ((num[j] as f64) * val[j] + (num[j - 1] as f64) * val[j - 1])
-                / ((num[j] + num[j - 1]) as f64);
-            num[j - 1] += num[j];
+        x[j] = values[i];
+        w[j] = weights[i];
+        while (j > 0) && x[j - 1].partial_cmp(&x[j]).unwrap() == ordering {
+            x[j - 1] = (w[j] * x[j] + w[j - 1] * x[j - 1]) / (w[j] + w[j - 1]);
+            w[j - 1] += w[j];
             j -= 1;
         }
+        s[j + 1] = i + 1;
     }
-    num.truncate(j + 1);
-    val.truncate(j + 1);
-    Regression { num, val }
+    w.truncate(j + 1);
+    x.truncate(j + 1);
+    s.truncate(j + 2);
+    // Map from new points to old points.
+    let mut ww = vec![0.0; values.len()];
+    let mut xx = vec![0.0; values.len()];
+    for k in 0..(j + 1) {
+        for i in s[k]..s[k + 1] {
+            ww[i] = w[k];
+            xx[i] = x[k];
+        }
+    }
+    Regression {
+        weights: ww,
+        values: xx,
+    }
 }
---
+++ tests/lib.rs
@@ -0,0 +1,90 @@
+extern crate pava;
+extern crate rand;
+extern crate svg;
+
+use rand::{Rng, SeedableRng, StdRng};
+use std::f64;
+use svg::node::element::Circle;
+use svg::Document;
+
+fn dump_svg(name: &str, values_in: &[f64], values_out: &[f64]) {
+    let (min_x, min_y, max_x, max_y) = values_in.iter().enumerate().fold(
+        (f64::MAX, f64::MAX, f64::MIN, f64::MIN),
+        |extrema, (x, &y)| {
+            (
+                extrema.0.min(x as f64),
+                extrema.1.min(y),
+                extrema.2.max(x as f64),
+                extrema.3.max(y),
+            )
+        },
+    );
+    let (min_x, min_y, max_x, max_y) =
+        values_out
+            .iter()
+            .enumerate()
+            .fold((min_x, min_y, max_x, max_y), |extrema, (x, &y)| {
+                (
+                    extrema.0.min(x as f64),
+                    extrema.1.min(y),
+                    extrema.2.max(x as f64),
+                    extrema.3.max(y),
+                )
+            });
+    let margin = 5.0;
+    let mut doc = Document::new().set(
+        "viewBox",
+        (
+            min_x - margin,
+            min_y - margin,
+            (max_x - min_x) + 2.0 * margin,
+            (max_y - min_y) + 2.0 * margin,
+        ),
+    );
+    for (x, &y) in values_in.iter().enumerate() {
+        let circle = Circle::new()
+            .set("fill", "black")
+            .set("r", 0.2)
+            .set("cx", x as f64)
+            .set("cy", max_y - y);
+        doc = doc.add(circle);
+    }
+    for (x, &y) in values_out.iter().enumerate() {
+        let circle = Circle::new()
+            .set("fill", "red")
+            .set("fill-opacity", "0.5")
+            .set("r", 0.4)
+            .set("cx", x as f64)
+            .set("cy", max_y - y);
+        doc = doc.add(circle);
+    }
+    svg::save(format!("target/_{}.svg", name), &doc).expect("Writing SVG failed");
+}
+
+fn noisy_line<F>(f: F) -> (Vec<f64>, Vec<f64>)
+where
+    F: Fn(usize, usize) -> usize,
+{
+    let mut seed = [0u8; 32];
+    seed.copy_from_slice((0..32).map(|i| i + 1).collect::<Vec<u8>>().as_slice());
+    let mut random: StdRng = SeedableRng::from_seed(seed);
+    let values: Vec<f64> = (0..100)
+        .map(|i| f(i, 100) as f64 * 0.5 + random.gen_range(-10.0, 10.0))
+        .collect();
+    let weights = vec![1.0; values.len()];
+    (values, weights)
+}
+
+#[test]
+fn increasing() {
+    let (values, weights) = noisy_line(|i, _| i);
+    let res = pava::pava(&values, &weights, std::cmp::Ordering::Greater);
+    dump_svg("increasing", &values, &res.values);
+}
+
+#[test]
+fn decreasing() {
+    let (values, weights) = noisy_line(|i, n| n - i);
+    let res = pava::pava(&values, &weights, std::cmp::Ordering::Less);
+    dump_svg("decreasing", &values, &res.values);
+}
